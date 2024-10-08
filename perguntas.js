aa242ed4f7c23845abacaf677f26ab141f7c453e
brunaslb <125621275+brunaslb@users.noreply.github.com>
2024-10-08 11:44:09 -0300

    a

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,13 @@
 criaCartao(
     'Matemática',
     'Como calcula a soma dos termos de uma P.A',
-    'Utiliza-se a formúla: ((a1+an).q/2'
+    'Utiliza-se a formúla: ((a1+an).q/2';
+)
+criaCartao(
+   'História',
+   'Quem foi o presidente do Brasil em 1938',
+   'Getúlio Vargas';
+)
+criaCartao(
+    
 )
