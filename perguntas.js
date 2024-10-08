bce159746be3fb38df1160629afefd603f2e3dff
brunaslb <125621275+brunaslb@users.noreply.github.com>
2024-10-08 12:16:59 -0300

    a

--- perguntas.js
+++ perguntas.js
@@ -9,5 +9,92 @@ criaCartao(
    'Getúlio Vargas';
 )
 criaCartao(
-    
+    'Português',
+    'O que é um substantivo?',
+    'Um substantivo é uma palavra que nomeia pessoas, lugares, coisas ou ideias. Por exemplo, "cachorro", "cidade" e "amor" são substantivos';
+)
+criaCartao(
+    'Biologia',
+    'O que é uma celula?',
+    'A célula é a menor unidade de um organismo, responsável por suas funções. Todos os seres vivos são compostos por células.';
+)
+criaCartao(
+    'Geografia',
+    'O que é um continente?',
+    ' Uma grande massa de terra cercada por oceanos';
+)
+criaCartao(
+    'Matematica', 
+    'O que é um número primo?',
+    'Um número que só pode ser dividido por 1 e por ele mesmo';
+)
+criaCartao(
+    'Química',
+    'O que é um átomo?',
+    'A menor unidade de um elemento químico';
+)
+criaCartao(
+    'Português',
+    'O que é um verbo?',
+    'Uma palavra que indica ação, estado ou ocorrência';
+)
+criaCartao(
+'Matematica',
+'O que é uma fração',
+'Uma parte de um todo, representada por dois números';
+)
+criaCartao(
+    'História',
+    'O que foi a Revolução Francesa?',
+    'Um movimento que derrubou a monarquia na França em 1789.';
+)
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'O processo em que as plantas transformam luz solar em energia.';
+)
+criaCartao(
+    'Geografia'
+    'O que é um rio?',
+    'Um curso de água que flui em direção ao mar.';
+)
+criaCartao(
+'Química'
+' O que é um elemento químico?';
+'Uma substância pura que não pode ser dividida em partes mais simples.';    
+)
+criaCartao(
+    'Física',
+    'O que é força?';
+    'Uma interação que pode mudar o movimento de um objeto.';
+)
+criaCartao(
+    'Literatura'
+    'O que é uma metáfora?',
+    'Uma comparação implícita entre duas coisas diferentes';
+)
+criaCartao(
+    'Arte'
+    'O que é uma pintura?',
+    'Uma obra de arte feita com tinta em uma superfície.';
+)
+criaCartao(
+    'Educação Física'
+    'O que é resistência?'
+    'A capacidade do corpo de sustentar atividades físicas por longos períodos';
+)
+criaCartao(
+    'Inglês'
+    'O que é um adjetivo?',
+    'Uma palavra que descreve um substantivo.';
+)
+criaCartao(
+    'Sociologia'
+    'O que é cultura?',
+    'O conjunto de costumes, crenças e valores de um grupo.';
+)
+criaCartao(
+    'Astronomia'
+'O que é um planeta?',
+'Um corpo celeste que orbita uma estrela'.;
 )
